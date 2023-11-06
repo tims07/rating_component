4759e379de289975e58095483824c4c998f51f0e
Timothy Sanders <timsanders07@gmail.com>
2023-11-06 10:47:19 -0500

    update logic and styling

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('hello')
+// Buttons
+
+const submitBtn = document.querySelector('.submit');
+const btnValue = document.querySelectorAll('.btn-value');
+const rateAgain = document.querySelector('.rate-again');
+const thanksContainer = document.querySelector('.thanks-container');
+const ratingsContainer = document.querySelector('.ratings-container');
+const rating = document.getElementById('rating')
+
+
+
+
+    btnValue.forEach(btn => {
+        btn.addEventListener('click', function(){
+            rating.innerHTML = btn.innerHTML;
+        })
+    })
+
+
+
+
+submitBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    ratingsContainer.classList.add('hidden')
+    thanksContainer.classList.remove('hidden');
+    
+})
+rateAgain.addEventListener('click', function(e){
+    e.preventDefault();
+    ratingsContainer.classList.remove('hidden')
+    thanksContainer.classList.add('hidden');
+   
+    
+})
+
+
